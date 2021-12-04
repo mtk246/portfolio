a1b730227591a2416e739a1fe0e7c2a7f616c55d
mtk246 <minthukyaw454@gmail.com>
2021-12-04 22:00:27 +0630

    Third Commit

--- template/body.php
+++ template/body.php
@@ -2,15 +2,15 @@
     <div class="container py-3 mx-auto my-3 text-center section-1-content">
         <img src="https://images.unsplash.com/photo-1549880181-56a44cf4a9a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8OHx8fGVufDB8fHx8&w=1000&q=80" class="port-img" alt="portfolio-image">
         <h1 class="display-5 fw-bolder">Min Thu Kyaw</h1>
-        <h3 class="fw-light">Senior PHP Developer</h3>
+        <h3 class="fw-light">FullStack Developer</h3>
         <a href="portfolio_pdf.php" class="btn">Show Resume</a>
     </div>
 </div>
 <div class="container-fluid section-2 pb-3">
     <div class="container py-3 mx-auto my-3 section-2-content">
         <h1 class="display-5 fw-bolder">About Me</h1>
-        <p class="fw-light">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis in consequatur, aperiam, cupiditate doloremque vero consequuntur blanditiis, quam hic esse vitae voluptatum totam expedita obcaecati enim itaque rem architecto officia.
+        <p class="fw-semibold">
+            I am interested in Programming, Web Development, and Web Design. I am a programmer with a passion for learning and problem solving. I am currently working as a PHP Developer at a local company. I am fluent in HTML, CSS, PHP, Vanilla JavaScript, jQuery, MySQL, Laravel, REST API and GIT. I mostly use Front-End Development with PHP, Javascript, jQuery, Bootstrap and sometimes React.js . And Back-End as Laravel,MySQL,REST API. I also familiar with AWS EC2 (Windows,Linux), RDS, Codecommit, Heroku Deploy, Heroku DBs (JawsDB,ClearDB). I am also familiar with Python, and Java.
         </p>
         <div class="container-fluid p-0">
             <div class="row">
@@ -33,10 +33,10 @@
                 <div class="col-md-6 my-2 card-col">
                     <div class="card shadow-md">
                         <div class="card-body">
-                            <h1 class="fs-4 fw-bold">Experience</h1>
+                            <h1 class="fs-4 fw-bold">Job Profile</h1>
                             <ul class="list-group list-group-numbered">
                                 <li class="list-group-item border-0">
-                                    <span class="position fw-bolder">Senior PHP Developer</span> at <span><a href="https://kopwar.com.mm" class="btn">Kopwar, Household Services</a></span><br /><span class="fs-6 fw-bold">(Feb 2020-Nov 2021)</span>
+                                    <span class="position fw-bolder">FullStack Developer</span> at <span><a href="https://kopwar.com.mm" class="btn">Kopwar, Household Services</a></span><br /><span class="fs-6 fw-bold">(Feb 2020-Nov 2021)</span>
                                 </li>
                                 <li class="list-group-item border-0">
                                     <span class="position fw-bolder">Freelancer</span> at <span><a href="https://khitkabarfooddistributor.business.site/" class="btn">Khit Kabar Food Distribution</a></span><br /><span class="fs-6 fw-bold">(Nov 2021-Present)</span>
@@ -55,10 +55,10 @@
         </p>
         <div class="container-fluid p-0">
             <div class="row">
-                <div class="col-12 my-2 card-col">
+                <div class="col-md-6 my-2 card-col">
                     <div class="card shadow-md">
                         <div class="card-body">
-                            <h1 class="fs-4 fw-bold">Social Media</h1>
+                            <h1 class="fs-4 fw-bold">Developer Media</h1>
                             <div class="container-fluid" style="overflow-x:scroll;">
                                 <ul class="list-group list-group-horizontal">
                                     <li class="list-group-item border-0">
@@ -66,6 +66,20 @@
                                     </li>
                                     <li class="list-group-item border-0">
                                         <a href="https://www.upwork.com/freelancers/~01914b5350fab0b1b2"><img src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-upwork-a-global-freelancing-platform-where-professionals-connect-and-collaborate-remotely-logo-shadow-tal-revivo.png" /></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 my-2 card-col">
+                    <div class="card shadow-md">
+                        <div class="card-body">
+                            <h1 class="fs-4 fw-bold">Social Media</h1>
+                            <div class="container-fluid" style="overflow-x:scroll;">
+                                <ul class="list-group list-group-horizontal">
+                                    <li class="list-group-item border-0">
+                                        <a href="tel:+959250048922"><img src="https://img.icons8.com/color/48/000000/phone.png" /></a>
                                     </li>
                                     <li class="list-group-item border-0">
                                         <a href="mailto:minthukyaw454@gmail.com"><img src="https://img.icons8.com/fluency/48/000000/gmail-new.png" /></a>
@@ -79,7 +93,6 @@
                                     <li class="list-group-item border-0">
                                         <a href="https://twitter.com/mtk246"><img src="https://img.icons8.com/fluency/48/000000/twitter.png" /></a>
                                     </li>
-
                                 </ul>
                             </div>
                         </div>
@@ -88,4 +101,8 @@
             </div>
         </div>
     </div>
+</div>
+<div class="footer container-fluid p-3">
+    <p class="fw-bolder">Source code available on <a href="https://github.com/mtk246/portfolio"><img src="https://img.icons8.com/fluency/48/000000/github.png" /></a>
+    </p>
 </div>
