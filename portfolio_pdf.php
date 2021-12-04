7ecc4a82d776cb11e1648eb008d6b4eaec2ad818
mtk246 <minthukyaw454@gmail.com>
2021-12-04 22:16:44 +0630

    Fourth Commit

--- portfolio_pdf.php
+++ portfolio_pdf.php
@@ -1,6 +1,8 @@
 <?php include_once("template/header.php"); ?>
-
-<iframe src="https://drive.google.com/file/d/1EkHxVgE_MsfZuuvnjjlXoq4aV6Gd3miX/preview" style="height: -webkit-fill-available;width: -webkit-fill-available">
+<div class="container m-2 p-2">
+    <a href="https://mtk-portfolio.herokuapp.com" class="btn"><img src="https://img.icons8.com/office/22/000000/home--v2.png" /> Back</a>
+    <a href="https://docs.google.com/document/d/1RnmMq6gn5ILy0L1LAUqaVUo58rPDuc59TuWb-xX3_EQ" class="btn">Show as DOCX</a>
+</div>
+<iframe src="https://docs.google.com/document/d/1RnmMq6gn5ILy0L1LAUqaVUo58rPDuc59TuWb-xX3_EQ/preview" style="height: -webkit-fill-available;width: -webkit-fill-available">
 </iframe>
-
 <?php include_once("template/footer.php"); ?>
