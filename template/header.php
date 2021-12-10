59595489a28609406b466af983fe38d2454f0eef
mtk246 <minthukyaw454@gmail.com>
2021-12-10 13:19:58 +0630

    meta theme-color added

--- template/header.php
+++ template/header.php
@@ -6,7 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="MTK Portfolio">
-
+    <meta name="theme-color" content="#ff5a3d" />
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#ff5a3d">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#ff5a3d">
     <title>Min Thu Kyaw</title>
 
     <!-- Bootstrap CSS CDN -->
