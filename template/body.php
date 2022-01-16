81aaad0a959d7f8bc296e73822965966d4b1f39e
mtk246 <minthukyaw454@gmail.com>
2022-01-16 17:57:28 +0630

    Commit

--- template/body.php
+++ template/body.php
@@ -1,7 +1,7 @@
-<button class="btn dark-button btn-mode"><svg class="mode-icon" fill="#6495ed" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+<button class="btn dark-button btn-mode" aria-label="Dark Mode"><svg class="mode-icon" fill="#6495ed" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
     </svg></button>
-<button class="btn light-button btn-mode" style="display:none;"><svg class="mode-icon" fill="#F9B329" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+<button class="btn light-button btn-mode" style="display:none;" aria-label="Light Mode"><svg class="mode-icon" fill="#F9B329" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z"></path>
     </svg></button>
 <div class="container-fluid section-1">
@@ -76,7 +76,10 @@
                                     <span class="position fw-bolder">FullStack Developer</span> at <span><a href="https://kopwar.com.mm" class="btn btn1">Kopwar, Household Services</a></span><br /><span class="fs-6 fw-bold">(Feb 2020-Nov 2021)</span>
                                 </li>
                                 <li class="list-group-item border-0">
-                                    <span class="position fw-bolder">Freelancer</span> at <span><a href="https://khitkabarfooddistributor.business.site/" class="btn btn1">Khit Kabar Food Distribution</a></span><br /><span class="fs-6 fw-bold">(Nov 2021-Present)</span>
+                                    <span class="position fw-bolder">Freelancer</span> at <span><a href="https://khitkabarfooddistributor.business.site/" class="btn btn1">Khit Kabar Food Distribution</a></span><br /><span class="fs-6 fw-bold">(Nov 2021-Dec 2021)</span>
+                                </li>
+                                <li class="list-group-item border-0">
+                                    <span class="position fw-bolder">Software Engineer</span> at <span><a href="https://cube-group.net" class="btn btn1">Cube Design & SNI</a></span><br /><span class="fs-6 fw-bold">(Jan 2021-Present)</span>
                                 </li>
                             </ul>
                         </div>
